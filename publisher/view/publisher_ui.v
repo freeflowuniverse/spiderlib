2bc2d9dbc728e8606d0bda920ff179e1531c36c0
timurgordon <timurgordon@gmail.com>
2023-01-02 22:16:32 +0300

    publisher auth changes

--- publisher/view/publisher_ui.v
+++ publisher/view/publisher_ui.v
@@ -1,59 +1,49 @@
 module view
 
 import vweb
-import time { Time }
+import time
 import os
 import freeflowuniverse.spiderlib.uikit.shell { Dashboard }
-import freeflowuniverse.crystallib.publisher2 { Publisher, User }
+import freeflowuniverse.spiderlib.auth.jwt
+import freeflowuniverse.crystallib.publisher2
 import freeflowuniverse.crystallib.pathlib
 
 const (
 	port = 8001
 )
 
-pub fn (mut app App) before_request() {
-	hx_request := app.get_header('Hx-Request') == 'true'
-	if !hx_request && app.req.url != '/' && app.req.url.ends_with('.html') && !app.req.url.starts_with('/sites/') {
-		app.redirect('')
-	}
-	// updates app user before each request
-	token := app.get_cookie('token') or { '' }
-	app.user = get_user(token) or { User{} }
+struct App {
+	vweb.Context
+mut:
+	username      string
+	email         string
+	attempted_url string
+	dashboard     Dashboard
 }
 
+pub fn (mut app App) before_request() {
+	// redirects to requests to index
+	hx_request := app.get_header('Hx-Request') == 'true'
+	if !hx_request && app.req.url != '/' && app.req.url.ends_with('.html')
+		&& !app.req.url.starts_with('/sites/') {
+		app.redirect('')
+	}
+
+	// updates app username before each request
+	token := app.get_cookie('token') or { '' }
+	app.username = jwt.get_data(token) or { '' }
+}
+
+// creates, initializes and returns app
 fn new_app() &App {
 	mut app := &App{}
 	app.mount_static_folder_at(os.resource_abs_path('view/static'), '/static')
 	return app
 }
 
-struct App {
-	vweb.Context
-mut:
-	user           User
-	email          string
-	attempted_url  string
-	dashboard      Dashboard
-	authenticators shared map[string]Auth
-	home           Home
-}
-
-struct AccessGroup {
-	name string
-}
-
-struct Auth {
-	max_attempts int = 3
-mut:
-	timeout       Time
-	auth_code     []u8
-	attempts      int  = 0
-	authenticated bool = false
-}
-
 pub fn run_view() {
 	mut app := new_app()
-	vweb.run(app, port)
+	vweb.run(app, view.port)
 }
 
 pub fn (mut app App) index() vweb.Result {
